fe395c926eb23f34889a151cd958f1b612995956
en32D <98673407+en32D@users.noreply.github.com>
2022-07-11 16:45:02 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+var a = 10;
+var b = 5;
+
+console.log(full_name);
+var add = a + b;
+var sub = a - b;
+var mult = a * b;
+console.log(add);
+console.log(sub);
+console.log(mult);
